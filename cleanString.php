d8163d4eff50a9c9665cd4de5d457599a3aca0a6
Peter 4A5A <498883@edu.rocmn.nl>
2017-09-07 11:17:45 +0200

    Added the assignment

---
+++ cleanString.php
@@ -0,0 +1,20 @@
+<?php
+  echo cleanString("HOI iK bEn PETER");
+
+  /**
+   * Cleans a string
+   * @param  [string] $string [A string that we want to clean]
+   * @return [string]         [The cleaned string]
+   */
+  function cleanString($string) {
+    $string = trim($string);
+    // Remove white space
+    $string = strtolower($string);
+    // All lower case
+    $string = ucwords($string);
+    // Upper case first letter
+    return($string);
+  }
+
+
+?>
